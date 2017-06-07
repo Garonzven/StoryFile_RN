2391d855d5422f766cac8cc5b4799e4d5eda0bcb
jeanmarie15 <jeanmarie.gonzalez@garonz.com>
2017-06-07 13:44:44 -0400

    Amanda View video size fixed, talk button updated

--- amanda.js
+++ amanda.js
@@ -18,31 +18,89 @@ import Button from 'apsl-react-native-button';
 import Video from 'react-native-video';
 
 export default class Amanda extends Component {
+
   constructor(props) {
      super(props);
      this.onLoad = this.onLoad.bind(this);
      this.onProgress = this.onProgress.bind(this);
+     this.onStartShouldSetResponder = this.onStartShouldSetResponder.bind(this);
+     this.onResponderRelease = this.onResponderRelease.bind(this);
   }
 
   state = {
- rate: 1,
- volume: 1,
- muted: true,
- resizeMode: 'contain',
- duration: 0.0,
- currentTime: 0.0,
+     rate: 1,
+     volume: 1,
+     muted: true,
+     resizeMode: 'cover',
+     duration: 0.0,
+     currentTime: 0.0,
+     talkText:'HOLD TO TALK',
+     square:{
+       width: '100%',
+       height: 60,
+       backgroundColor: '#2AB999',
+       bottom:0,
+       position:'absolute'
+     },circle: {
+         width: 40,
+         height: 40,
+         borderRadius: 50,
+         backgroundColor: '#2AB999',
+         bottom:40,
+         position:'absolute'
+       },
    };
 
   onLoad(data) {
- this.setState({duration: data.duration});
+      this.setState({duration: data.duration});
    }
 
   onProgress(data) {
- this.setState({currentTime: data.currentTime});
+      this.setState({currentTime: data.currentTime});
+  }
+
+  onStartShouldSetResponder(){
+    console.warn("start should set responder");
+    this.setState({square:{
+      width: '100%',
+      height: 60,
+      backgroundColor: '#E74D3D',
+      bottom:0,
+      position:'absolute'
+    },circle: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        backgroundColor: '#E74D3D',
+        bottom:40,
+        position:'absolute'
+      },
+    talkText:'RELEASE TO LISTEN'})
+    return true;
+  }
+
+  onResponderRelease(){
+    console.warn("responder release");
+    this.setState({square:{
+      width: '100%',
+      height: 60,
+      backgroundColor: '#2AB999',
+      bottom:0,
+      position:'absolute'
+    },circle: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        backgroundColor: '#2AB999',
+        bottom:40,
+        position:'absolute'
+      },
+    talkText:'HOLD TO TALK'})
   }
 
 
   render() {
+    var color_talk = '';
     return (
       <View style={styles.container}>
       <Video source={require('./Videos/amanda_bird_active_listening.mp4')}
@@ -55,15 +113,22 @@ export default class Amanda extends Component {
     onLoad={() => {console.warn("loading done");}}
     onProgress={this.onProgress}
     playInBackground={true}
-    onEnd={() => { this.refs.myVideo.refs.node.seek(0) }} repeat={true}
+    repeat={true}
       />
 
 
 
-        <View style={styles.circle} />
+        <View style={this.state.circle} />
 
-        <View style={styles.square} >
-            <Text style={styles.talk}>  HOLD TO TALK</Text>
+        <View style={this.state.square}
+              accessible={true}
+              onAccessibilityTap={() => {console.warn("tap");}}
+              onStartShouldSetResponder={this.onStartShouldSetResponder}
+              onMoveShouldSetResponder={() => {console.warn("move should set responder");}}
+              onResponderRelease={this.onResponderRelease}
+              onMagicTap={() => {console.warn("MagicTap");}}
+              >
+            <Text style={styles.talk}> {this.state.talkText}</Text>
         </View>
         <Image source={require('./Sprites/micxxhdpi.png')} style={styles.mic}  ></Image>
 
@@ -79,21 +144,6 @@ const styles = StyleSheet.create({
     alignItems:'center',
     justifyContent:'center',
     backgroundColor: '#2D3941'
-  },
-  square:{
-    width: '100%',
-    height: 60,
-    backgroundColor: '#2AB999',
-    bottom:0,
-    position:'absolute'
-  },
-  circle: {
-    width: 40,
-    height: 40,
-    borderRadius: 50,
-    backgroundColor: '#2AB999',
-    bottom:40,
-    position:'absolute'
   },
   mic:{
     position: 'absolute',
